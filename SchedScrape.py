566778e92d3a04894b4c7a5f65bb111f626d128a
swiggumj <swiggumj@gmail.com>
2020-08-19 12:17:32 -0500

    blackened

--- SchedScrape.py
+++ SchedScrape.py
@@ -12,7 +12,12 @@ import pytz
 from datetime import datetime, timedelta
 import argparse
 
-aoDictP2780 = {"(a)": "Session A", "(b)": "Session B", "(c)": "Session C", "(d)": "Session D"}
+aoDictP2780 = {
+    "(a)": "Session A",
+    "(b)": "Session B",
+    "(c)": "Session C",
+    "(d)": "Session D",
+}
 
 aoDictP2945 = {
     "(a)": "0030",
@@ -24,17 +29,20 @@ aoDictP2945 = {
     "(e)+(a)": "2317,0030",
 }
 
-obscode_dict = {'0':'F-1400',
-                '1':'A-1400',
-                '2':'A-820',
-                '3':'B-1400',
-                '4':'B-820',
-                '5':'C-1400',
-                '6':'C-820',
-                '7':'D-1400',
-                '8':'D-820',
-                '9':'E-1400',
-                '10':'E-820'}
+obscode_dict = {
+    "0": "F-1400",
+    "1": "A-1400",
+    "2": "A-820",
+    "3": "B-1400",
+    "4": "B-820",
+    "5": "C-1400",
+    "6": "C-820",
+    "7": "D-1400",
+    "8": "D-820",
+    "9": "E-1400",
+    "10": "E-820",
+}
+
 
 class Sched:
     """Observatory schedule class
@@ -110,8 +118,8 @@ class Sched:
         """
 
         self.SessID = []
-        for pid,rsid in zip(self.ProjID,self.RawSessID):
-        
+        for pid, rsid in zip(self.ProjID, self.RawSessID):
+
             if "2780" in pid:
                 self.SessID.append(aoDictP2780[rsid])
             elif "2945" in pid:
@@ -120,10 +128,13 @@ class Sched:
                 try:
                     self.SessID.append(aoDictP2780[rsid])
                 except KeyError:
-                    print('Could not match session key, %s. Adding empty string...' % (rsid))
-                    self.SessID.append('')
+                    print(
+                        "Could not match session key, %s. Adding empty string..."
+                        % (rsid)
+                    )
+                    self.SessID.append("")
                 except:
-                    print('Blurgh. Something else happened.') 
+                    print("Blurgh. Something else happened.")
 
     def ObsTimesUTC(self):
         """
@@ -162,7 +173,7 @@ class Sched:
             )
             self.WikiLines.append(WikiLine)
 
-    def PrintWikiLines(self,all=False,reverse=True):
+    def PrintWikiLines(self, all=False, reverse=True):
         """
         Prints schedule lines to copy directly to the wiki. By default, only future
         sessions are printed (all=False) with the latest date on top (reverse=True).
@@ -170,10 +181,10 @@ class Sched:
 
         if not all:
             # Find UTC start times after Time.now()
-            TimeNow = Time.now() 
+            TimeNow = Time.now()
             FutureInds = np.where(self.StartUTC > TimeNow)
             OutLines = self.WikiLines[FutureInds]
-            
+
         else:
             OutLines = self.WikiLines
 
@@ -182,7 +193,7 @@ class Sched:
         else:
             [print(wl) for wl in OutLines]
 
-    def PrintInfoDefault(self,all=False,reverse=True):
+    def PrintInfoDefault(self, all=False, reverse=True):
         """
         Prints relevant scheduling info as CSV.
             [ProjID], [SessID], [Start MJD], [StartLoc], [EndLoc]
@@ -191,18 +202,27 @@ class Sched:
         """
         StartMJD = ["%.2f" % (Time(ut).mjd) for ut in np.flip(self.StartUTC)]
 
-        #if not all:
+        # if not all:
         #    # Find UTC start times after Time.now()
         #    TimeNow = Time.now()
         #    FutureInds = np.where(self.StartUTC > TimeNow)
         #    OutLines = self.WikiLines[FutureInds]
 
         for i in range(self.nRows):
-            print("%s, %s, %s, %s, %s" %
-                (self.ProjID[i],self.SessID[i],StartMJD[i],self.StartLoc[i],self.EndLoc[i]))
+            print(
+                "%s, %s, %s, %s, %s"
+                % (
+                    self.ProjID[i],
+                    self.SessID[i],
+                    StartMJD[i],
+                    self.StartLoc[i],
+                    self.EndLoc[i],
+                )
+            )
+
 
 def get_session(id):
-    sess_str = obscode_dict[str(int(id)%11)]
+    sess_str = obscode_dict[str(int(id) % 11)]
     return sess_str
 
 
@@ -214,19 +234,20 @@ def ValidProjID(ProjID):
 
     List of supported projects now contained in SupportedProjIDs.list.
     """
-    SupportedProjIDs = np.loadtxt('SupportedProjIDs.list',dtype='str')
-    return (ProjID in SupportedProjIDs)
+    SupportedProjIDs = np.loadtxt("SupportedProjIDs.list", dtype="str")
+    return ProjID in SupportedProjIDs
+
 
 def DetermineTelescope(ProjID):
     """
     Determine telescope associated with input project ID.
     """
-    if 'GBT' in ProjID:
-        telescope = 'GBT'
-    elif ('P' in ProjID) or ('X' in ProjID):
-        telescope = 'AO'
+    if "GBT" in ProjID:
+        telescope = "GBT"
+    elif ("P" in ProjID) or ("X" in ProjID):
+        telescope = "AO"
     else:
-        print('No telescope associated with project: %s' % (ProjID))
+        print("No telescope associated with project: %s" % (ProjID))
         sys.exit()
 
     return telescope
@@ -234,9 +255,9 @@ def DetermineTelescope(ProjID):
 
 def ScrapeSchedGBO(project, year):
 
-    page = requests.get('https://dss.gb.nrao.edu/schedule/public')
-    soup = BeautifulSoup(page.content,'html.parser')
-    table = soup.findChildren('table')[1]
+    page = requests.get("https://dss.gb.nrao.edu/schedule/public")
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.findChildren("table")[1]
 
     # Calculate local AO start/end times from SchedTable
     GBO = pytz.timezone("US/Eastern")
@@ -246,49 +267,49 @@ def ScrapeSchedGBO(project, year):
     StartList = []
     EndList = []
     WrapList = []
-    for rr in table.findChildren('tr'):
-         if not rr.a:
-             date_str = rr.contents[1].text.split()[0]
-         else:
-             proj_str = rr.a['title']
+    for rr in table.findChildren("tr"):
+        if not rr.a:
+            date_str = rr.contents[1].text.split()[0]
+        else:
+            proj_str = rr.a["title"]
 
-             if (project in proj_str):
-                 wrap = 0
-                 proj_id = proj_str.split(' - ')[0].strip()
-                 sess_id = proj_str.split(' - ')[1].strip()
+            if project in proj_str:
+                wrap = 0
+                proj_id = proj_str.split(" - ")[0].strip()
+                sess_id = proj_str.split(" - ")[1].strip()
 
-                 obs_elems = rr.findChildren('td')
-                 time_window = obs_elems[0].text.strip()
-                 start_et_str = time_window.split(' - ')[0].strip()    #.replace('+','')
-                 end_et_str = time_window.split(' - ')[1].strip()    #.replace('+','')
+                obs_elems = rr.findChildren("td")
+                time_window = obs_elems[0].text.strip()
+                start_et_str = time_window.split(" - ")[0].strip()  # .replace('+','')
+                end_et_str = time_window.split(" - ")[1].strip()  # .replace('+','')
 
-                 if '+' in end_et_str:
-                     start = '%s %s' % (date_str,start_et_str.replace('+',''))
-                 else:
-                     if '+' in start_et_str:
-                         end = '%s %s' % (date_str,end_et_str.replace('+',''))
-                         wrap = 1
-                     else:
-                         start = '%s %s' % (date_str,start_et_str)
-                         end = '%s %s' % (date_str,end_et_str)
-   
-                     t0 = GBO.localize(datetime.strptime(start, '%Y-%m-%d %H:%M'))
-                     t1 = GBO.localize(datetime.strptime(end, '%Y-%m-%d %H:%M'))
-                     ProjList.append(proj_id)
-                     SessList.append(sess_id)
-                     StartList.append(t0)
-                     EndList.append(t1)
-                     WrapList.append(wrap)
+                if "+" in end_et_str:
+                    start = "%s %s" % (date_str, start_et_str.replace("+", ""))
+                else:
+                    if "+" in start_et_str:
+                        end = "%s %s" % (date_str, end_et_str.replace("+", ""))
+                        wrap = 1
+                    else:
+                        start = "%s %s" % (date_str, start_et_str)
+                        end = "%s %s" % (date_str, end_et_str)
+
+                    t0 = GBO.localize(datetime.strptime(start, "%Y-%m-%d %H:%M"))
+                    t1 = GBO.localize(datetime.strptime(end, "%Y-%m-%d %H:%M"))
+                    ProjList.append(proj_id)
+                    SessList.append(sess_id)
+                    StartList.append(t0)
+                    EndList.append(t1)
+                    WrapList.append(wrap)
 
     SchedTable = Table(
-        [ProjList,SessList,StartList,EndList,WrapList],    
-        names=('Proj','Sess','StartLocal','EndLocal','DayWrap')
+        [ProjList, SessList, StartList, EndList, WrapList],
+        names=("Proj", "Sess", "StartLocal", "EndLocal", "DayWrap"),
     )
 
     # Sort table, eventually this tag shouldn't be needed. Cludge fix for now.
-    SortTag = np.array([int(datetime.strftime(st,'%Y%m%d%H%M')) for st in StartList])
-    SchedTable['SortTag'] = SortTag
-    SchedTable.sort(keys=['SortTag'])
+    SortTag = np.array([int(datetime.strftime(st, "%Y%m%d%H%M")) for st in StartList])
+    SchedTable["SortTag"] = SortTag
+    SchedTable.sort(keys=["SortTag"])
 
     return SchedTable
 
@@ -340,60 +361,60 @@ def ScrapeSchedAO(project, year):
     AO = pytz.timezone("America/Puerto_Rico")
 
     BlockDates = np.array(
-        [
-            AO.localize(datetime.strptime(ds, "%b_%d_%y"))
-            for ds in SchedTable["DateStr"]
-        ]
+        [AO.localize(datetime.strptime(ds, "%b_%d_%y")) for ds in SchedTable["DateStr"]]
     )
     StartAO = np.array(
         [
             bd + timedelta(days=1.0 * c, minutes=15.0 * r)
-            for bd, c, r in zip(
-                BlockDates, SchedTable["BegCol"], SchedTable["BegRow"]
-            )
+            for bd, c, r in zip(BlockDates, SchedTable["BegCol"], SchedTable["BegRow"])
         ]
     )
     EndAO = np.array(
         [
             bd + timedelta(days=1.0 * c, minutes=15.0 * r)
-            for bd, c, r in zip(
-                BlockDates, SchedTable["EndCol"], SchedTable["EndRow"]
-            )
+            for bd, c, r in zip(BlockDates, SchedTable["EndCol"], SchedTable["EndRow"])
         ]
     )
 
     # Clean up the table to remove extraneous info, add datetimes
-    SchedTable.remove_columns([
-        'DateStr','StartLST','EndLST','TimeSys',
-        'BegCol','EndCol','BegRow','EndRow','Hours'
-    ])
+    SchedTable.remove_columns(
+        [
+            "DateStr",
+            "StartLST",
+            "EndLST",
+            "TimeSys",
+            "BegCol",
+            "EndCol",
+            "BegRow",
+            "EndRow",
+            "Hours",
+        ]
+    )
 
-    SchedTable['StartLocal'] = StartAO
-    SchedTable['EndLocal'] = EndAO
+    SchedTable["StartLocal"] = StartAO
+    SchedTable["EndLocal"] = EndAO
 
-    SortTag = np.array([int(datetime.strftime(st,'%Y%m%d%H%M')) for st in StartAO])
-    SchedTable['SortTag'] = SortTag
+    SortTag = np.array([int(datetime.strftime(st, "%Y%m%d%H%M")) for st in StartAO])
+    SchedTable["SortTag"] = SortTag
 
-    # Sort the table by SortTag, ...do I still need this column? 
-    SchedTable.sort(keys=['SortTag'])
+    # Sort the table by SortTag, ...do I still need this column?
+    SchedTable.sort(keys=["SortTag"])
 
-    SchedTable['DayWrap'] = np.zeros(len(SchedTable))
+    SchedTable["DayWrap"] = np.zeros(len(SchedTable))
     RemoveRows = []
-    for r in range(len(SchedTable)-1):
-        
+    for r in range(len(SchedTable) - 1):
+
         # Merge sessions continuing over a day boundary
-        if (
-            (SchedTable['EndLocal'][r] == SchedTable['StartLocal'][r+1]) and
-            (SchedTable['Sess'][r] == SchedTable['Sess'][r+1])
+        if (SchedTable["EndLocal"][r] == SchedTable["StartLocal"][r + 1]) and (
+            SchedTable["Sess"][r] == SchedTable["Sess"][r + 1]
         ):
-            SchedTable['EndLocal'][r] = SchedTable['EndLocal'][r+1]
-            SchedTable['DayWrap'][r] = 1
-            RemoveRows.append(r+1)
-        
+            SchedTable["EndLocal"][r] = SchedTable["EndLocal"][r + 1]
+            SchedTable["DayWrap"][r] = 1
+            RemoveRows.append(r + 1)
+
         # Merge adjacent P2945 sessions
-        elif (
-            (SchedTable['EndLocal'][r] == SchedTable['StartLocal'][r+1]) and
-            (SchedTable['Proj'][r] == SchedTable['Proj'][r+1] == 'P2945')
+        elif (SchedTable["EndLocal"][r] == SchedTable["StartLocal"][r + 1]) and (
+            SchedTable["Proj"][r] == SchedTable["Proj"][r + 1] == "P2945"
         ):
             pass
 
@@ -412,12 +433,7 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "--projects",
-        "-p",
-        type=str,
-        nargs="+",
-        required=True,
-        help="Project code(s)",
+        "--projects", "-p", type=str, nargs="+", required=True, help="Project code(s)",
     )
     parser.add_argument(
         "--year",
@@ -431,44 +447,43 @@ def main():
         "--all",
         "-a",
         action="store_true",
-        help="Print all sessions in the chosen year."
+        help="Print all sessions in the chosen year.",
     )
     parser.add_argument(
-        "--reverse",
-        "-r",
-        action="store_true",
-        help="Print sessions in reverse order."
+        "--reverse", "-r", action="store_true", help="Print sessions in reverse order."
     )
     parser.add_argument(
         "--printformat",
         "-pf",
-        choices=['wiki','none','gbncc','default'],
+        choices=["wiki", "none", "gbncc", "default"],
         nargs="?",
-        default='default',
-        help="Format of schedule info printed."
+        default="default",
+        help="Format of schedule info printed.",
     )
 
     # To print usage if no arguments are provided.
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help()
         parser.exit()
 
     args = parser.parse_args()
-    projects = [str(item) for item in args.projects[0].split(',')]
-    
+    projects = [str(item) for item in args.projects[0].split(",")]
+
     SchedTables = []
     for p in projects:
 
         if ValidProjID(p):
             Telescope = DetermineTelescope(p)
 
-            if Telescope == 'GBT':
+            if Telescope == "GBT":
                 SchedTables.append(ScrapeSchedGBO(p, args.year))
-            elif Telescope == 'AO':
+            elif Telescope == "AO":
                 SchedTables.append(ScrapeSchedAO(p, args.year))
         else:
-            print('Invalid project: %s' % (p))
-            print('Try fiddling with case or -/_; automatic fixes for these coming soon...')
+            print("Invalid project: %s" % (p))
+            print(
+                "Try fiddling with case or -/_; automatic fixes for these coming soon..."
+            )
             sys.exit()
 
     FullSched = vstack(SchedTables)
@@ -479,6 +494,6 @@ def main():
     else:
         x.PrintInfoDefault()
 
+
 if __name__ == "__main__":
     main()
-
